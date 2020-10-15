6d3cc88e4b17879f39389939c1cdec1083d869bc
kattyTimm <katya.timofeevva@gmail.com>
2020-10-15 17:00:56 +0300

    12

---
+++ src/components/Profile.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+const Profile = () => {
+  return (
+      <div className="content">    
+        <img src="https://img3.goodfon.ru/original/6000x4000/4/dd/abstract-colors-unreal-clouds-1927.jpg" />
+
+        <div>
+           ava+description
+        </div>
+
+        <div>
+           My posts      
+           <div>
+              New posts
+           </div>
+
+           <div className="posts">
+              <div>fake post 1</div>
+              <div>fake post 2</div>
+           </div>
+
+        </div>  
+
+      </div> 
+
+  );
+}
+
+export default Profile;
